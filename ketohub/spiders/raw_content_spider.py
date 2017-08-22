2a7441f2fcd9687d22b48689315a157d34bf703b
fernandalavalle <fernanda.lavalle8@gmail.com>
2017-08-22 15:06:44 -0400

    Deleted stub files from scrapy, added requirement text files, and ran pylint and yapf over everything.

---
+++ ketohub/spiders/raw_content_spider.py
@@ -0,0 +1,11 @@
+""" Defines the spider class, RawContentSpider for the project ketohub.
+"""
+import scrapy
+
+class RawContentSpider(scrapy.Spider):
+    """ Dummy spider class. """
+    name = "raw_content"
+    allowed_domains = []
+
+    def parse(self, response):
+        return
