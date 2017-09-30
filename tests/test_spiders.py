f93482c9b5cdbf950231b29dc09cd34cc154d4b8
Michael Lynch <mtlynch@users.noreply.github.com>
2017-09-30 13:01:28 -0400

    Consolidating spiders into a single file (#22)

--- tests/test_spiders.py
+++ tests/test_spiders.py
@@ -5,36 +5,33 @@ import unittest
 import mock
 from scrapy import http
 
-from ketohub.spiders import raw_content_spider
+from ketohub import spiders
 
 
-class RawContentSpiderTest(unittest.TestCase):
+class SpiderBaseTest(unittest.TestCase):
     """Test case for the raw_content spider."""
 
     def setUp(self):
         mock_urlopen = mock.patch(
-            'ketohub.spiders.raw_content_spider.urllib.urlopen', autospec=True)
+            'ketohub.spiders.urllib.urlopen', autospec=True)
         self.addCleanup(mock_urlopen.stop)
         self.urlopen_patch = mock_urlopen.start()
 
         self.mock_start_scrape_time = datetime.datetime(
             year=2017, month=1, day=2, hour=3, minute=4, second=5)
-        mock_datetime = mock.patch(
-            'ketohub.spiders.raw_content_spider.datetime.datetime')
+        mock_datetime = mock.patch('ketohub.spiders.datetime.datetime')
         self.addCleanup(mock_datetime.stop)
         datetime_patch = mock_datetime.start()
         datetime_patch.utcnow.return_value = self.mock_start_scrape_time
 
-        mock_content_saver = mock.patch(
-            'ketohub.spiders.raw_content_spider.persist.ContentSaver')
+        mock_content_saver = mock.patch('ketohub.spiders.persist.ContentSaver')
         self.addCleanup(mock_content_saver.stop)
         self.content_saver_patch = mock_content_saver.start()
         self.mock_saver = mock.Mock()
         self.content_saver_patch.return_value = self.mock_saver
 
         mock_get_recipe_main_image = mock.patch(
-            'ketohub.spiders.raw_content_spider.RawContentSpider._get_recipe_main_image_url'
-        )
+            'ketohub.spiders.SpiderBase._get_recipe_main_image_url')
         self.addCleanup(mock_get_recipe_main_image.stop)
         self.get_image_patch = mock_get_recipe_main_image.start()
 
@@ -47,7 +44,7 @@ class RawContentSpiderTest(unittest.TestCase):
 
         self.get_image_patch.return_value = 'https://mock.com/test_image.jpg'
         self.urlopen_patch.return_value = io.BytesIO('dummy image data')
-        spider = raw_content_spider.RawContentSpider()
+        spider = spiders.SpiderBase()
         spider.download_recipe_contents(response)
 
         self.content_saver_patch.assert_called_once_with(
@@ -71,7 +68,47 @@ class RawContentSpiderTest(unittest.TestCase):
             body='')
 
         self.get_image_patch.side_effect = IndexError
-        spider = raw_content_spider.RawContentSpider()
+        spider = spiders.SpiderBase()
 
-        with self.assertRaises(raw_content_spider.UnexpectedResponse):
+        with self.assertRaises(spiders.UnexpectedResponse):
             spider.download_recipe_contents(response)
+
+
+class KetoConnectSpiderTest(SpiderBaseTest):
+    """Test case for the ketoconnect_raw_content spider."""
+
+    def test_get_recipe_main_image_url_returns_second_image(self):
+        """Tests that the correct second image is extracted."""
+        file_content = (
+            "<html><img src='images/wrong_image.jpg'><img src='images/right_image.jpg'></html>"
+        )
+
+        response = http.TextResponse(
+            url='https://www.foo.com',
+            request=http.Request('https://www.foo.com'),
+            body=file_content)
+
+        spider = spiders.KetoConnectSpider()
+        spider.download_recipe_contents(response)
+
+        self.urlopen_patch.assert_called_with('images/right_image.jpg')
+
+
+class RuledMeSpiderTest(SpiderBaseTest):
+    """Test case for the ruled_me_raw_content spider."""
+
+    def test_get_recipe_main_image_url_returns_first_image(self):
+        """Tests that the first image location is extracted."""
+        file_content = (
+            "<html><img src='images/right_image.jpg'><img src='images/wrong_image.jpg'></html>"
+        )
+
+        response = http.TextResponse(
+            url='https://www.foo.com',
+            request=http.Request('https://www.foo.com'),
+            body=file_content)
+
+        spider = spiders.RuledMeSpider()
+        spider.download_recipe_contents(response)
+
+        self.urlopen_patch.assert_called_once_with('images/right_image.jpg')
