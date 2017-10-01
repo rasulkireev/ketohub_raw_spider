ee4e502dd3abea3699527edf761ac082f548617e
Michael Lynch <michael@ketohub.io>
2017-09-30 22:30:25 -0400

    Return error if content type != 'image/jpeg'
    Fixes #17

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,4 +1,3 @@
-import io
 import unittest
 import urllib2
 
@@ -16,15 +15,27 @@ class ImagesTest(unittest.TestCase):
         self.urlopen_patch = mock_urlopen.start()
 
     def test_download_succeeds_when_server_is_ok(self):
-        self.urlopen_patch.return_value = io.BytesIO('dummy image data')
+        mock_handle = mock.Mock()
+        mock_handle.info.return_value = mock.Mock(type='image/jpeg')
+        mock_handle.read.return_value = 'dummy image data'
+        self.urlopen_patch.return_value = mock_handle
         self.assertEqual(
             images.download_data('http://mock.com/image.jpg'),
             'dummy image data')
         self.urlopen_patch.assert_called_once_with('http://mock.com/image.jpg')
 
-    def test_download_succeeds_when_server_returns_403(self):
+    def test_download_fails_when_server_returns_403(self):
         self.urlopen_patch.side_effect = urllib2.HTTPError(
             url='', code=404, msg='', hdrs=None, fp=None)
         with self.assertRaises(urllib2.HTTPError):
+            images.download_data('http://mock.com/image.jpg')
+        self.urlopen_patch.assert_called_once_with('http://mock.com/image.jpg')
+
+    def test_download_fails_when_content_type_is_not_jpeg(self):
+        mock_handle = mock.Mock()
+        mock_handle.info.return_value = mock.Mock(type='image/png')
+        mock_handle.read.return_value = 'dummy image data'
+        self.urlopen_patch.return_value = mock_handle
+        with self.assertRaises(images.UnexpectedImageType):
             images.download_data('http://mock.com/image.jpg')
         self.urlopen_patch.assert_called_once_with('http://mock.com/image.jpg')
