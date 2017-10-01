98f18988eed45e63231dfd5182fdf75f8073f979
Michael Lynch <mtlynch@users.noreply.github.com>
2017-09-30 22:32:42 -0400

    Return error if content type != 'image/jpeg' (#25)
    Fixes #17

--- ketohub/images.py
+++ ketohub/images.py
@@ -1,6 +1,18 @@
 import urllib2
 
 
+class Error(Exception):
+    pass
+
+
+class UnexpectedImageType(Error):
+    pass
+
+
 def download_data(url):
     image_handle = urllib2.urlopen(url)
+    if image_handle.info().type != 'image/jpeg':
+        print 'type: %s' % image_handle.info().type
+        raise UnexpectedImageType('Expected image/jpeg, got ' +
+                                  image_handle.info().type)
     return image_handle.read()
