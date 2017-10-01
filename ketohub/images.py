951f69f3f9459da49336bed4b57a7844aa3af219
Michael Lynch <mtlynch@users.noreply.github.com>
2017-09-30 22:58:42 -0400

    Removing debug printing from images (#27)

--- ketohub/images.py
+++ ketohub/images.py
@@ -12,7 +12,6 @@ class UnexpectedImageType(Error):
 def download_data(url):
     image_handle = urllib2.urlopen(url)
     if image_handle.info().type != 'image/jpeg':
-        print 'type: %s' % image_handle.info().type
         raise UnexpectedImageType('Expected image/jpeg, got ' +
                                   image_handle.info().type)
     return image_handle.read()
