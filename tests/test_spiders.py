501a94bc38dc0f4b7ebfaa31874518f72b46f989
Michael Lynch <michael@ketohub.io>
2017-09-30 23:09:20 -0400

    Raise an error when image url is not found

--- tests/test_spiders.py
+++ tests/test_spiders.py
@@ -18,6 +18,14 @@ class FindKetoConnectImageUrlTest(unittest.TestCase):
   <meta property="og:image" content="https://mock.com/recipe-image.jpg" />
 </html>""")), 'https://mock.com/recipe-image.jpg')
 
+    def test_raises_error_when_image_not_found(self):
+        with self.assertRaises(spiders.NoImageFound):
+            spiders.find_ketoconnect_image_url(
+                http.TextResponse(
+                    url='https://www.foo.com',
+                    request=http.Request('https://www.foo.com'),
+                    body='<html></html>'))
+
 
 class FindRuledMeImageUrlTest(unittest.TestCase):
 
@@ -31,3 +39,11 @@ class FindRuledMeImageUrlTest(unittest.TestCase):
 <html>
   <meta property="og:image" content="https://mock.com/recipe-image.jpg" />
 </html>""")), 'https://mock.com/recipe-image.jpg')
+
+    def test_raises_error_when_image_not_found(self):
+        with self.assertRaises(spiders.NoImageFound):
+            spiders.find_ruled_me_image_url(
+                http.TextResponse(
+                    url='https://www.foo.com',
+                    request=http.Request('https://www.foo.com'),
+                    body='<html></html>'))
