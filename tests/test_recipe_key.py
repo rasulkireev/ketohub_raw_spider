4990011e8603cd3c372f0038dbc55f6ffd4d2fa4
Michael Lynch <mtlynch@users.noreply.github.com>
2017-10-30 19:09:57 -0400

    Fixing recipe_key to handle URLS without subdomains (#40)

--- tests/test_recipe_key.py
+++ tests/test_recipe_key.py
@@ -9,3 +9,13 @@ class RawContentSpiderTest(unittest.TestCase):
         self.assertEqual(
             recipe_key.from_url('https://www.mock.com/Mikes_Chicken_Kiev/'),
             'mock-com_mikes-chicken-kiev')
+
+    def test_from_url_handles_no_subdomain(self):
+        self.assertEqual(
+            recipe_key.from_url('https://mock.com/Mikes_Chicken_Kiev/'),
+            'mock-com_mikes-chicken-kiev')
+
+    def test_from_url_handles_http(self):
+        self.assertEqual(
+            recipe_key.from_url('http://www.mock.com/Mikes_Chicken_Kiev/'),
+            'mock-com_mikes-chicken-kiev')
