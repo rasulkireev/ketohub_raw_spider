f192d04721f3c2a40d4d1d1715e4feed36bfba41
Michael Lynch <git@mtlynch.io>
2019-02-15 10:40:16 -0500

    Adding a Keto Diet App spider

--- ketohub/settings.py
+++ ketohub/settings.py
@@ -1,8 +1,4 @@
-"""Scrapy settings for ketohub project.
-
-http://doc.scrapy.org/en/latest/topics/settings.html
-"""
-
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"
 BOT_NAME = 'ketohub'
 
 SPIDER_MODULES = ['ketohub.spiders']
